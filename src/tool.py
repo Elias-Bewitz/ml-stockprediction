0c44d310f1655de2d23af47c2c5d31ffca21ff20
SwimelBee <elias.bewitz@swimbird.se>
2025-07-22 16:34:02 +0200

    test

--- src/tool.py
+++ src/tool.py
@@ -2,7 +2,7 @@ import yfinance as yf
 
 stock = yf.Ticker("AAPL")
 
-historical_stock_data = stock.history(period="1y")
+historical_stock_data = stock.history(period="500y")
 
 with open("historical_data.txt", "w") as file:
     file.write(historical_stock_data.to_string())
